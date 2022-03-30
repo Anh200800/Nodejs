2e3e49bc0998e0cf595154e88b6b09959e80939a
Your Name <you@example.com>
2022-03-30 23:57:07 +0700

    first commit

---
+++ src/config/viewEngine.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { append } from 'express/lib/response';
+let conFigViewEngine = (app)=> {
+app.use(express.static('./src/public'));
+app.set('view engine', 'ejs');
+app.set('views', './src/views')
+}
+module.exports = conFigViewEngine;
